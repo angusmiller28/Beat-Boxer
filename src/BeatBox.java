f9f1cb7f16ede3183865bbfd68fa045c3c4c4098
Angus Kevin Miller <angus.miller28@gmail.com>
2017-03-19 14:52:56 +1000

    Implemented MidiEvent method
    Implemented the MidiEvent method which creates a new instrument.

--- src/BeatBox.java
+++ src/BeatBox.java
@@ -92,6 +92,12 @@ public class BeatBox {
     
     public MidiEvent makeEvent(int comd, int chan, int one, int two, int tick){
        MidiEvent event = null;
+       try {
+        ShortMessage a = new ShortMessage();
+        a.setMessage(comd, chan, one, two);
+        event = new MidiEvent(a, tick);
+        
+    } catch(Exception e){ e.printStackTrace(); }
        return event;
     }
 } // close outer class
